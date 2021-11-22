998d975e780938d289525754b1ddf585bf6cd68e
Gil Junqueira <gjunqueira@msn.com>
2021-11-22 11:47:40 -0600

    Added dot function

--- src/LinearAlgebraStudy.jl
+++ src/LinearAlgebraStudy.jl
@@ -3,7 +3,7 @@ using Symbolics
 using SymbolicUtils
 using Plots
 
-export drawVector
+export drawVector, dot
 
 """
     drawVector(V, origin = [0,0])
@@ -31,6 +31,43 @@ export drawVector
 function drawVector(V, origin = [0,0], xlim=5, ylim=5)
     plot!([origin[1], V[1]+origin[1]], [origin[2], V[2]+origin[2]],  xlims = [0,xlim], ylims = [0,ylim], line=:arrow)
 
+end
+
+
+
+
+
+
+"""
+    dot(V₁::Vector, V₂::Vector)
+    Computes the dot product of two vectors.
+
+    # Parameters
+    V₁ : Vector
+        The first vector.
+    V₂ : Vector
+        The second vector.
+
+    # Returns
+    dot : 
+        The dot product of the two vectors.
+
+    # Example
+   ```julia
+    using LinearAlgebraStudy
+    V1 = Vector([1,2])
+    V2 = Vector([3,4])
+    dot(V1, V2)
+    ```
+
+"""
+function dot(V₁::Vector, V₂::Vector)
+    if length(V₁) == length(V₂)
+        return V₁'*V₂
+    else error("Vectors must be of the same length")
+    end
+        
+   
 end
 
 
