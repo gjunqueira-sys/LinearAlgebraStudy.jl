874bb3ff192c768fcfc70acc8413665407b2b0bc
giljunqueira-max <83184039+giljunqueira-max@users.noreply.github.com>
2022-06-04 16:51:03 -0500

    added basis transf in 4d

--- src/LinearAlgebraStudy.jl
+++ src/LinearAlgebraStudy.jl
@@ -5,7 +5,7 @@ using Plots
 using LinearAlgebra
 
 
-export drawVector, testVectorsIndependance, transformVectorBasis2D
+export drawVector, testVectorsIndependance, transformVectorBasis2D, transformVectorBasis4D
 
 
 
@@ -99,7 +99,7 @@ end
             ```
 
     # Parameters
-    P::Vector
+    b::Vector
         Vector to be transformed.
     BX::Vector
         X-component of the new basis.
@@ -118,15 +118,71 @@ end
 
 
 """
-function transformVectorBasis2D(P::Vector, BX::Vector, BY::Vector)
+function transformVectorBasis2D(b::Vector, BX::Vector, BY::Vector)
     A = [BX BY] # Matrix A with new basis vectors as columns
-    b = P # Vector b with original coordinates of point P.
+    
     c = A\b # Solve for new coordinates on new Basis corresponding to original coordinates point P.
 
     return c
 end
 
+"""
+    transformVectorBasis2D(P::Vector, BX::Vector, BY::Vector) 
+        Function to transform a given Vector P point to a new Basis. Original Basis is assumed to be
+        the standard cartesian basis.
+        The transformed Vector is the equivalent of the original Vector P in the new Basis.
 
+        Vectors can be described in terms of scalar multiplication of the basis vectors:
+        Example:
+            P=[2,1] is the original Vector with standard cartersian basis (X=[1,0],Y=[0,1]).
+            This can be written as linear combination of the basis vectors:
+                P=2*X+1*Y
+            
+            If we want to transform this Vector to a new basis (X'=[1,1],Y'=[0,2]), we can write the
+            new vector on the new basis as a linear combination and then solve for the new scalar coefficients:
+                
+             ```
+            
+            P'=c₁*X'+ c₂*Y'  ⟹ c₁ * [1,1] + c₂ * [0,2] = [2,1]
+
+            let A = [1 0   # Matrix A with new basis vectors
+                     1 2]
+            
+            let b = [2 1] # Vector b with original coordinates of point P.
+
+            let c = Vector of new coordinates on new Basis corresponding to original coordinates point P.
+
+                c = A\b
+
+            ```
+
+    # Parameters
+    b::Vector
+        Vector to be transformed.
+    BX::Vector
+        X-component of the new basis.
+    BY::Vector
+        Y-component of the new basis.
+
+    # Returns
+    p::Vector
+        Transformed vector.
+
+    # Examples
+
+    
+    
+        
+
+
+"""
+function transformVectorBasis4D(b::Vector, BX::Vector, BY::Vector, BZ::Vector, BW::Vector)
+    A = [BX BY BZ BW] # Matrix A with new basis vectors as columns
+    
+    c = A\b # Solve for new coordinates on new Basis corresponding to original coordinates point P.
+
+    return c
+end
 
 
 
